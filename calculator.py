00d29a2d65cfc31a019657f267393d344250f535
Teddy Juntunen <tjuntunen@Teds-MacBook-Pro.local>
2017-04-11 18:08:18 -0700

    initial commit

---
+++ calculator.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+
+def homepage():
+	html = ''
+	html += '<html>\n'
+	html += '<body>\n'
+	html += '<form align="center" method="POST" action="/calculate" name="form">\n'
+	html += '<input placeholder="1st Number" name="firstNumber">'
+	html += '<input placeholder="2nd Number" name="secondNumber">\n'
+	html += '<input placeholder="Operand" name="operand">\n'
+	html += '<input type="submit" value="Submit">\n'
+	html += '</form>\n'
+	html += '</body>\n'
+	html += '</html>\n'
+	return html
+
+@app.route('/calculate', methods=['GET', 'POST'])
+
+def calculate():
+	firstNum = request.form['firstNumber']
+	secondNum = request.form['secondNumber']
+	operand = request.form['operand']
+
+	if operand != "+" and operand != "-" and operand != "*" and operand != "/":
+		answer = '<p>Invalid operand. <a href="/">Click here to go back and try again</p></a>'
+	if operand == "+":
+		answer = str(int(firstNum) + int(secondNum))
+	if operand == "-":
+		answer = str(int(firstNum) - int(secondNum))
+	if operand == "*":
+		answer = str(int(firstNum) * int(secondNum))
+	if operand == "/":
+		answer = str(int(firstNum) / int(secondNum))
+	return answer
+
+if __name__ == '__main__':
+	app.run()
---
+++ wordfreq.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def homepage():
+    html = ''
+    html += '<html>\n'
+    html += '<body>\n'
+    html += '<form method="POST" action="/calculate">'
+    html += '<input type="text" placeholder="Enter text here" name="text">'
+    html += '<input type="submit" value="Submit">'
+    html += '</form>'
+    html += '</body>\n'
+    html += '</html>\n'
+    return html
+
+@app.route('/calculate', methods=['GET', 'POST'])
+
+def calculate():
+    wordDictionary = {}
+    text = request.form['text']
+    processedText = text.lower().split()
+    for word in processedText:
+        if word not in wordDictionary:
+            wordDictionary[word] = 0
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+    html = ''
+    html += '<table border="1">'
+    html += '<th>Word</th>'
+    html += '<th>Count</th>'
+    for i in wordDictionary.keys():
+        html += '<tr>'
+        html += '<td>' + str(i) + '</td>'
+        html += '<td>' + str(wordDictionary[i]) + '</td>'
+        html += '</tr>'
+    html += '</table>'
+    return html
+
+if __name__ == '__main__':
+    app.run()
